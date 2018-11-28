fb43228ce91b5d7106574d70a1b3c6c44e19b6d3
Yesung_Han <930805hys@gmail.com>
2018-11-28 11:36:37 +0900

    1111
    111

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from bookmark.models import Bookmark
 
 # Register your models here.
-
+#dddd
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('title', 'url')
 
